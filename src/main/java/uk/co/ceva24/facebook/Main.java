f011bfdedf2398abcc3d9e2c318d0ce368918aee
Chris E <chris@ceva24.dev>
2016-03-28 18:36:39 +0100

    initial commit

---
+++ src/main/java/uk/co/ceva24/facebook/Main.java
@@ -0,0 +1,67 @@
+package uk.co.ceva24.facebook;
+
+import uk.co.ceva24.facebook.model.FacebookObject;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+public class Main
+{
+    private static Facebook facebook;
+
+    public static void main(String[] args)
+    {
+        try
+        {
+            facebook = new Facebook();
+
+            Main.runNameQuery();
+            Main.runLikesQuery();
+            Main.runDegreesQuery();
+        }
+        catch (KeyManagementException | NoSuchAlgorithmException e)
+        {
+            System.err.println("Failed to create Facebook client. Cause: " + e.getMessage());
+        }
+    }
+
+    private static void runNameQuery()
+    {
+        System.out.println(facebook.getNameForId("ceva24"));
+    }
+
+    private static void runLikesQuery()
+    {
+        for (FacebookObject like : facebook.getLikes("cocacola"))
+            System.out.println(like);
+    }
+
+    private static void runDegreesQuery()
+    {
+        FacebookObject[] degrees = facebook.getSixDegrees("cocacola");
+
+        for (int i = 0; i < 6; i++)
+        {
+            FacebookObject o = degrees[i]; 
+
+            if (o == null)
+            {
+                String suffix;
+                switch (i)
+                {
+                    case 1  : suffix = "st"; break;
+                    case 2  : suffix = "nd"; break;
+                    case 3  : suffix = "rd"; break;
+                    default : suffix = "th"; break;
+                }
+
+                System.out.println("You only reached the " + (i) + suffix + " degree! Better luck next time!");
+                break;
+            }
+            else
+            {
+                System.out.println(o);
+            }
+        }
+    }
+}
---
+++ src/test/java/uk/co/ceva24/facebook/FacebookTests.java
@@ -0,0 +1,59 @@
+package uk.co.ceva24.facebook;
+
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.Test;
+import uk.co.ceva24.facebook.model.FacebookObject;
+
+public class FacebookTests
+{
+    private static final String userName = "ceva24";
+    private static final String pageName = "cocacola";
+
+    private Facebook facebook;
+
+    public FacebookTests() {}
+
+    @Before
+    public void setUp()
+    {
+        try
+        {
+            facebook = new Facebook();
+        }
+        catch (KeyManagementException | NoSuchAlgorithmException e)
+        {
+            System.err.println("Failed to create Facebook client. Cause: " + e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    @Test
+    public void testGetNameForId()
+    {
+        String name = facebook.getNameForId(userName);
+        assertEquals("Unexpected username value returned", "Christian Evans", name);
+    }
+
+    @Test
+    public void testGetLikes()
+    {
+        List<FacebookObject> likes = facebook.getLikes(pageName);
+        assertTrue("No data returned", likes.size() > 0);
+    }
+
+    @Test
+    public void testGetSixDegrees()
+    {
+        FacebookObject[] degrees = facebook.getSixDegrees(pageName);
+
+        assertEquals("Unexpected array length", 6, degrees.length);
+        assertFalse("Control object " + pageName + " returned no likes", degrees[0] == null);
+    }
+}
